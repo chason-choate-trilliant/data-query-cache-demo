9aa4e15d415a19d443498b4ef6e6074982da5a73
Chason Choate <125305848+chason-choate-trilliant@users.noreply.github.com>
2025-04-05 10:15:00 -0500

    Testing POST 303 approach

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,11 +1,13 @@
 import "./style.css";
 
-import { setupCounter } from "./counter";
 import typescriptLogo from "./typescript.svg";
 
 async function requestData(path: string, method: string) {
   const res = await fetch(path, {
     method,
+    headers: {
+      "Authentication": "Bearer token"
+    }
   })
   const data = await res.json()
   return data
@@ -29,31 +31,18 @@ enableMocking().then(() => {
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
     <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-    <button id="server-data-get" type="button">Get Server Data</button>
     <button id="server-data-post" type="button">Post Server Data</button>
     <button id="mock-data" type="button">Get Mock Data</button>
   </div>
 `;
 
-  document.querySelector("#server-data-get")!.addEventListener("click", async () => {
-    const data = await requestData("/server", "get")
-    console.log(data)
-  })
   document.querySelector("#server-data-post")!.addEventListener("click", async () => {
-    const data = await requestData("/server", "post")
+    const data = await requestData("/search:create", "post")
     console.log(data)
   })
   document.querySelector("#mock-data")!.addEventListener("click", async () => {
     const data = await requestData("/mock", "get")
     console.log(data)
   })
-
-  setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
 })
 
--- src/server/main.ts
+++ src/server/main.ts
@@ -3,19 +3,24 @@ import ViteExpress from "vite-express";
 
 const app = express();
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + TypeScript!");
+app.post("/search:create", (req, res)  => {
+  console.log("POST /search")
+  const newURL = URL.parse('http://localhost:3000/search:execute')
+  newURL.searchParams.append('q', btoa(req.body))
+  console.log("Redirecting to:", newURL.toString())
+  res.redirect(303, newURL.toString())
 });
-
-app.get("/server", (req, res)  => {
+app.get("/search:execute", async (req, res)  => {
+  console.log("GET /search")
+  if (req.header('If-None-Match') === '12345') {
+    console.log("304 Not Modified")
+    res.status(304).send()
+    return
+  }
+  await new Promise(resolve => setTimeout(resolve, 5000))
   res.setHeader('Cache-Control', 'private,no-cache,max-age=60');
   res.setHeader('Etag', '12345');
-  res.send({ one: 'test', two: 'test '});
-});
-app.post("/server", (req, res)  => {
-  res.setHeader('Cache-Control', 'private,no-cache,max-age=60');
-  res.setHeader('Etag', '12345');
-  res.send({ one: 'test', two: 'test '});
+  res.send({ one: 'test', two: 'test' })
 });
 
 ViteExpress.listen(app, 3000, () =>
